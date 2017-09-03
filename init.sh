8b5230f45b10ebee49aa79bb54fe2c13431624e9
youfeng <youfeng@haizhi.com>
2017-09-03 20:44:44 +0800

    修改脚本

--- init.sh
+++ init.sh
@@ -6,5 +6,7 @@ virtualenv ~/venv
 cp -rf .env.sample .env
 export $(cat .env | xargs)
 
-nohup ~/venv/bin/python manage.py runserver -h 0.0.0.0 --port 5000 > nohup.txt 2>&1 &
-echo "启动完成..."
+
+
+~/venv/bin/python manage.py syncdb
+echo "数据库初始化完成..."
