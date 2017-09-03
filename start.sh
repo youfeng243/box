3e0ba15326f3356549e52f978cc27ac43f46b31c
youfeng <youfeng@haizhi.com>
2017-09-03 20:41:50 +0800

    修改启动脚本

--- start.sh
+++ start.sh
@@ -6,4 +6,4 @@ virtualenv ~/venv
 cp -rf .env.sample .env
 export $(cat .env | xargs)
 
-nohup ~/venv/bin/python manage.py runserver -h 0.0.0.0 > nohup.txt 2>&1 &
+nohup ~/venv/bin/python manage.py runserver -h 0.0.0.0 --port 5000 > nohup.txt 2>&1 &
